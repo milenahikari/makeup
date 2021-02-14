fb1b86495901fa0f29144d12d0d754f88eb8cd83
RgMellon <renan_gian@hotmail.com>
2021-02-14 19:17:10 -0300

    Create header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { SimpleLineIcons } from '@expo/vector-icons';
+import React, { useState } from "react";
+import { SimpleLineIcons } from "@expo/vector-icons";
 
-import * as S from './styles';
+import * as S from "./styles";
 
 const Home = () => {
-  return <S.Header>
-    <S.HeaderLeft>
-      <S.StoreName>MakeUp</S.StoreName>
-    </S.HeaderLeft>
-    <S.HeaderRight>
+  const [search, setSearch] = useState(null);
 
-    </S.HeaderRight>
+  return (
+    <S.Header>
+      <S.HeaderDetail>
+        <S.HeaderLeft>
+          <S.StoreName>MakeUp</S.StoreName>
+        </S.HeaderLeft>
+        <S.HeaderRight />
+      </S.HeaderDetail>
 
-    {/* <SimpleLineIcons name="menu" size={32} color="#232129" /> */}
-  </S.Header>;
-}
+      <S.InputSearch
+        placeholder="Search..."
+        value={search}
+        onChangeText={(search) => setSearch(search)}
+      />
+      {/* <SimpleLineIcons name="menu" size={32} color="#232129" /> */}
+    </S.Header>
+  );
+};
 
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,23 +1,44 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
 export const Header = styled.View`
+  position: relative;
   width: 100%;
-  height: 100px;
+  height: 200px;
+  align-items: center;
+`;
+
+export const HeaderDetail = styled.View`
+  width: 100%;
+  height: 100%;
   flex-direction: row;
 `;
 
 export const HeaderLeft = styled.View`
-  padding-top: 40px;
+  width: 50%;
   padding-left: 20px;
-  padding-right: 20px;
-  padding-bottom: 20px;
-  background: #d4d6d7;
+  padding-top: 50px;
 `;
 
-export const HeaderRight = styled.View``;
-
 export const StoreName = styled.Text`
-  font-family: 'Roboto_500Medium';
+  font-family: "Roboto_500Medium";
   font-size: 25px;
   color: #232129; //312e38
+`;
+
+export const HeaderRight = styled.View`
+  width: 50%;
+  background: #f6f7fa;
+`;
+
+export const InputSearch = styled.TextInput`
+  position: absolute;
+  width: 80%;
+  height: 45px;
+  padding-left: 10px;
+  margin-top: 120px;
+  background: #fff;
+  color: #bac7d3;
+  border: 1px;
+  border-color: #f6f7fa;
+  border-radius: 5px;
 `;
